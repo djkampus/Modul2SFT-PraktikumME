6bcc14da03699a57354af5173b9bb85a5d5e1c89
dzakyraffy <dzakyale-dzakly@yahoo.co.id>
2021-03-09 14:38:11 +0700

    Update Modul2SFT.m
    plottingnya ngaco nggak tau kenapa
    tapi sempet kesave yang bener plotnya

--- Modul2SFT.m
+++ Modul2SFT.m
@@ -1,8 +1,7 @@
 %%%preparation
 [y,Fs]=audioread('Spesifikasi B.wav');
 dep=(1-0.94);%prosentase nilai susutan
-L=2000;
- %%%plotting isyarat masukan x(t) dimana adalah sample audio
+%%%plotting isyarat masukan x(t) dimana adalah sample audio
 %%%dalam ranah waktu
 figure(1)
 plot(y)
@@ -10,13 +9,15 @@ title('Source Signal x(t) in Time Domain')
 xlabel('t (s)')
 ylabel('amplitude')
 %%dalam ranah frekuensi
-
+%%dalam ranah frekuensi
+L=length(y);
 y_fft_single = fftshift(fft(y));%sinyal yang ingin di fft-kan
 y_fft_single = y_fft_single(L/2+1:end);
 y_fft_single = abs(2*y_fft_single)./L;
 axis_single = (0:L/2)*(Fs/L);               %%x axis for freq domain
 figure(2)
-plot(axis_single, y_fft_magnitude_single);   %plot signal in freq domain
+plot(f, y_fft_single);   %plot signal in freq domain
 title('Source Signal x(t) in Frequency Domain');
 xlabel('frequency (Hz)');
 ylabel('magnitude');
+
